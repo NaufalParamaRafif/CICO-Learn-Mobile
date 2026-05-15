1e53f8caf7a9391e6f89289360a9f399ef3b448c
NaufalParamaRafif <naufalrama135@gmail.com>
2026-05-16 00:53:38 +0700

    Add announcement component and streak card in homepage

--- app/src/main/java/com/example/cicolearn/MainActivity.kt
+++ app/src/main/java/com/example/cicolearn/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,9 +19,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -28,6 +33,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -35,6 +42,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -42,7 +50,9 @@ import androidx.compose.ui.unit.sp
 import com.example.cicolearn.ui.theme.CICOLearnTheme
 import com.example.cicolearn.ui.theme.LightBackground
 import com.example.cicolearn.ui.theme.LightDarkBorder
+import com.example.cicolearn.ui.theme.LightDarkText
 import com.example.cicolearn.ui.theme.LightLightBorder
+import com.example.cicolearn.ui.theme.LightLightText
 import com.example.cicolearn.ui.theme.LightOnPrimary
 import com.example.cicolearn.ui.theme.LightPrimary
 import com.example.cicolearn.ui.theme.LightSecondary
@@ -63,7 +73,69 @@ class MainActivity : ComponentActivity() {
                     bottomBar = {
                         BottomAppBarHomepage()
                     },
-                ) {
+                ) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+//                            .background(color = Color.Red)
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                            .padding(horizontal = 16.dp),
+                        verticalArrangement = Arrangement.Top,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Spacer(Modifier.height(8.dp))
+                        Row(
+                        ) {
+                            Text(String(Character.toChars(128536)), fontSize = 32.sp)
+                            Spacer(Modifier.width(16.dp))
+                            Text("Hello ..! Mumpung otak masih fresh nih, mau belajar apa?", fontWeight = FontWeight.SemiBold, fontSize = 14.sp, color = LightDarkText)
+                        }
+                        Spacer(Modifier.height(16.dp))
+                        ElevatedCard(
+                            colors = CardColors(
+                                containerColor = LightPrimary,
+                                contentColor = LightOnPrimary,
+                                disabledContentColor = Color.Green,
+                                disabledContainerColor = Color.Yellow
+                            ),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        ) {
+                            Column(
+                                modifier = Modifier.padding(horizontal = 24.dp, vertical = 24.dp)
+                            ) {
+                                Text("Streak", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.SpaceBetween,
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Text("3 days")
+                                    Text("121 lessons")
+                                }
+                                Spacer(Modifier.height(16.dp))
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    horizontalArrangement = Arrangement.SpaceBetween
+                                ) {
+                                    Icon(painter = painterResource(R.drawable.streak_fire_off), contentDescription = null)
+                                    Icon(painter = painterResource(R.drawable.streak_fire_off), contentDescription = null)
+                                    Icon(painter = painterResource(R.drawable.streak_fire_off), contentDescription = null)
+                                    Icon(painter = painterResource(R.drawable.streak_fire_off), contentDescription = null)
+                                    Icon(painter = painterResource(R.drawable.streak_fire_off), contentDescription = null)
+                                    Icon(painter = painterResource(R.drawable.streak_fire_off), contentDescription = null)
+                                    Icon(painter = painterResource(R.drawable.streak_fire_off), contentDescription = null)
+                                }
+                            }
+                        }
+                        Spacer(Modifier.height(24.dp))
+                        Row(
+//                            modifier = Modifier.horizontalScroll()
+                        ) {
+
+                        }
+                    }
                 }
             }
         }
@@ -75,7 +147,9 @@ class MainActivity : ComponentActivity() {
 fun TopAppBarHomepage() {
     var input by rememberSaveable { mutableStateOf("") }
     TopAppBar(
-        modifier = Modifier.background(color = LightBackground),
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = LightBackground
+        ),
         title = {
             BasicTextField(
                 value = input,
@@ -107,6 +181,7 @@ fun TopAppBarHomepage() {
         actions = {
             Surface(
                 modifier = Modifier.size(44.dp),
+                color = Color.Transparent
             ) {
                 IconButton (
                     onClick = {}
--- app/src/main/java/com/example/cicolearn/ui/theme/Color.kt
+++ app/src/main/java/com/example/cicolearn/ui/theme/Color.kt
@@ -1,5 +1,7 @@
 package com.example.cicolearn.ui.theme
 
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -13,6 +15,7 @@ val Pink40 = Color(0xFF7D5260)
 // Light Custom Color
 val LightBackground = Color(0xFFF1F5FD)
 val LightPrimary = Color(0xFF306EE8)
+val LightPrimaryDark = Color(0xFF144BB8)
 val LightOnPrimary = Color(0xFFE8EFFC)
 val LightSecondary = Color(0xFFF2F2F2)
 val LightSurface = Color(0xFFFAFCFE)
@@ -21,3 +24,9 @@ val LightLightText = Color(0xFF94A0B8)
 val LightDarkBorder = Color(0xFF667799)
 val LightLightBorder = Color(0xFFB7C0D1)
 val LightBottomNavigationLightText = Color(0xFF5C6B8A)
+
+val LightPrimaryGradient = Brush.linearGradient(
+    colors = listOf(LightPrimary, LightPrimaryDark),
+    start = Offset(0f, Float.POSITIVE_INFINITY),
+    end = Offset(Float.POSITIVE_INFINITY, 0f)
+)
