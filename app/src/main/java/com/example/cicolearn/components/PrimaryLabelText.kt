56056393b32e5a2ba07e3da9d00bbf53565727b9
NaufalParamaRafif <naufalrama135@gmail.com>
2026-05-26 16:05:20 +0700

    Add Memorize choose mode and history screen

--- app/src/main/java/com/example/cicolearn/components/PrimaryLabelText.kt
+++ app/src/main/java/com/example/cicolearn/components/PrimaryLabelText.kt
@@ -1,11 +1,14 @@
 package com.example.cicolearn.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.cicolearn.ui.theme.LightOnPrimary
@@ -15,9 +18,15 @@ import com.example.cicolearn.ui.theme.LightPrimary
 fun PrimaryLabelText(text: String) {
     Text(
         text,
+        style = TextStyle(
+            platformStyle = PlatformTextStyle(
+                includeFontPadding = false
+            )
+        ),
         modifier = Modifier
             .background(LightPrimary, shape = RoundedCornerShape(100.dp))
-            .padding(horizontal = 8.dp),
+            .padding(horizontal = 8.dp, vertical = 0.8.dp),
+//            .height(16.dp),
         color = LightOnPrimary,
         fontSize = 12.sp
     )
---
+++ app/src/main/java/com/example/cicolearn/screen/MemorizeChooseModeScreen.kt
@@ -0,0 +1,87 @@
+package com.example.cicolearn.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.cicolearn.components.PrimaryGradientElevatedButton
+import com.example.cicolearn.components.TopAppBarBack
+import com.example.cicolearn.ui.theme.LightBackground
+import com.example.cicolearn.ui.theme.LightDarkText
+import com.example.cicolearn.ui.theme.LightOnPrimary
+import com.example.cicolearn.ui.theme.LightPrimaryGradient
+
+// TODO: REFACTOR THIS SCREEN
+@Composable
+fun MemorizeChooseModeScreen(navController: NavController) {
+    Scaffold(
+        containerColor = LightBackground,
+        topBar = {
+            TopAppBarBack(
+                navController,
+                "Memorize Vocabulary"
+            )
+        }
+    ) { contentPadding ->
+        Column(
+            modifier = Modifier.padding(contentPadding).fillMaxSize().padding(horizontal = 24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Choose between this one!", fontSize = 16.sp, fontWeight = FontWeight.Bold, color = LightDarkText, modifier = Modifier.fillMaxWidth())
+
+            Spacer(Modifier.height(20.dp))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(LightPrimaryGradient, RoundedCornerShape(4.dp))
+                    .padding(horizontal = 24.dp),
+                contentAlignment = Alignment.Center,
+            ) {
+                Text("Memorize All Vocabulary", modifier = Modifier.padding(12.dp), fontWeight = FontWeight.Bold, fontSize = 16.sp, color = LightOnPrimary)
+            }
+
+            Spacer(Modifier.height(20.dp))
+
+            Text("OR", fontSize = 16.sp, fontWeight = FontWeight.Bold, textAlign = TextAlign.Center, color = LightDarkText, modifier = Modifier.fillMaxWidth())
+
+            Spacer(Modifier.height(20.dp))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(LightPrimaryGradient, RoundedCornerShape(4.dp))
+                    .padding(horizontal = 16.dp),
+                contentAlignment = Alignment.Center,
+            ) {
+                Text("Memorize System's Recommended Vocab", modifier = Modifier.padding(12.dp), textAlign = TextAlign.Center, fontWeight = FontWeight.Bold, fontSize = 16.sp, color = LightOnPrimary)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun MemorizeChooseModeScreenPreview() {
+    val navController = rememberNavController()
+    MemorizeChooseModeScreen(navController)
+}
